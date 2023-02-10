641107fb4f23fd08e2b276c38ef678449468d147
Jclock <jacopo.clocchiatti@gmail.com>
2023-02-10 13:36:00 +0100

    Add medium model, new videos, updated file with argparse and switch between track with camshift or fully yolo

--- test_yolov8.py
+++ test_yolov8.py
@@ -1,3 +1,7 @@
+import argparse
+from typing import List, Dict
+
+from numpy import ndarray
 from ultralytics import YOLO
 
 from kalmanfilter import KalmanFilter
@@ -7,16 +11,19 @@ MAX_FRAMES = 1000000
 CUT_FACTOR = 7
 REDU = 8
 THRESHOLD = 60
+FRAMES_INTERVAL = 24
 
 
-def init_tracker():
-    return YOLO("utils/best_s.pt")
+def init_tracker(model_type: str) -> YOLO:
+    load_model = YOLO(f"models/best_{model_type}.pt")
+    load_model.to("cpu")
+    return load_model
 
 
-def init_kalman():
+def init_kalman(fps: int) -> (ndarray, ndarray, ndarray, ndarray, ndarray, ndarray, ndarray, List, ndarray, ndarray):
+
     kernel = np.ones((3, 3), np.uint8)
 
-    fps = 120
     dt = 1 / fps
     noise = 3
 
@@ -41,24 +48,32 @@ def init_kalman():
     # x, y, vx, vy
     mu = np.array([0, 0, 0, 0])
     P = np.diag([10, 10, 10, 10]) ** 2
+
     res = []
-    N = 15
     sigmaM = 0.0001
     sigmaZ = 3 * noise
 
     Q = sigmaM ** 2 * np.eye(4)
     R = sigmaZ ** 2 * np.eye(2)
 
-    return kernel, A, B, u, H, mu, P, res, N, Q, R
+    return kernel, A, B, u, H, mu, P, res, Q, R
 
 
-def track_trajectory(input_file):
+def get_bbox_coords(results: Dict) -> (int, int, int, int):
+    bbox = results[0].boxes.xyxy.numpy()[0]
+    x_t_l, y_t_l, x_b_r, y_b_r = list(map(lambda x: int(x), bbox))
+    x_b_r -= CUT_FACTOR
+    y_b_r -= CUT_FACTOR
+    x_t_l += CUT_FACTOR
+    y_t_l += CUT_FACTOR
+    return x_t_l, y_t_l, x_b_r, y_b_r
+
+
+def track_trajectory(input_file: str, type_of_model: str, camshift: bool, write: bool, show: bool):
     bgsub = cv2.createBackgroundSubtractorMOG2(500, 60, True)
-
-    model = init_tracker()
+    model = init_tracker(type_of_model)
 
     found = False
-    add_count = 0
     clean = False
     fps = 120
 
@@ -66,23 +81,32 @@ def track_trajectory(input_file):
 
     kf = KalmanFilter()
 
-    kernel, A, B, u, H, mu, P, res, N, Q, R = init_kalman()
+    kernel, A, B, u, H, mu, P, res, Q, R = init_kalman(fps)
 
+    # lists of boxes' centre
     listCenterX = []
     listCenterY = []
+    frames = []
 
     cap = cv2.VideoCapture(input_file)
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    frame_num = 0
 
-    while True:  # for t in range(MAX_FRAMES):
+    while True:
         # Capture frame-by-frame
-        key = cv2.waitKey(15) & 0xFF
+        key = cv2.waitKey(5) & 0xFF
         if key == 27:
             break
 
+        # read frame
         ret, frame = cap.read()
+        frame_num += 1
         # If video end reached
         if not ret:
             break
+
+        # compute color mask
         bgs = bgsub.apply(frame)
         bgs = cv2.erode(bgs, kernel, iterations=1)
         bgs = cv2.medianBlur(bgs, 3)
@@ -94,42 +118,52 @@ def track_trajectory(input_file):
         if not found:
             print("Detecting ball...")
             results = model(frame)
-            cv2.imshow('Frame', frame)
+            if show:
+                cv2.imshow('Frame', frame)
+
             if results[0].boxes.shape[0] == 0:
                 continue
             else:
                 found = True
                 print("Ball found...")
-                bbox = results[0].boxes.xyxy.cpu().numpy()[0]
-                x_t_l, y_t_l, x_b_r, y_b_r = list(map(lambda x: int(x), bbox))
-                x_b_r -= CUT_FACTOR
-                y_b_r -= CUT_FACTOR
-                x_t_l += CUT_FACTOR
-                y_t_l += CUT_FACTOR
-                imCrop = frame[y_t_l:y_b_r, x_t_l:x_b_r]
+                x_t_l, y_t_l, x_b_r, y_b_r = get_bbox_coords(results)
+                selection = frame[y_t_l:y_b_r, x_t_l:x_b_r]
                 print("Cropping ball...")
-                imCropMask = cv2.cvtColor(imCrop, cv2.COLOR_BGR2GRAY)
-                ret, imCropMask = cv2.threshold(imCropMask, 30, 255, cv2.THRESH_BINARY)
-                his = smooth(1, rgbh([imCrop], imCropMask))
-                roiBox = (x_t_l, y_t_l, x_b_r - x_t_l, y_b_r - y_t_l)
+                if camshift:
+                    selection_mask = cv2.cvtColor(selection, cv2.COLOR_BGR2GRAY)
+                    ret, selection_mask = cv2.threshold(selection_mask, 30, 255, cv2.THRESH_BINARY)
+                    his = smooth(1, rgbh([selection], selection_mask))
+                selection_box = (x_t_l, y_t_l, x_b_r - x_t_l, y_b_r - y_t_l)
                 print("Done")
         else:
-            print("Starting to track the ball...")
-            add_count += 1
-            rgbr = np.floor_divide(colorMask, REDU)
-            r, g, b = rgbr.transpose(2, 0, 1)
-            l = his[r, g, b]
-
             print("Track ball...")
-            (rb, roiBox) = cv2.CamShift(l, roiBox, termination)
+            if camshift:
+                rgbr = np.floor_divide(colorMask, REDU)
+                r, g, b = rgbr.transpose(2, 0, 1)
+                l = his[r, g, b]
+                (rb, selection_box) = cv2.CamShift(l, selection_box, termination)
+                cv2.ellipse(frame, rb, (0, 255, 0), 2)
+                error = (selection_box[3])
+                conf = 1
+            else:
+                results = model(frame)
+                if results[0].boxes.shape[0] != 0:
+                    print("Ball found...")
+                    x_t_l, y_t_l, x_b_r, y_b_r = get_bbox_coords(results)
+                    selection_box = (x_t_l, y_t_l, x_b_r - x_t_l, y_b_r - y_t_l)
+                    conf = results[0].boxes[0].cpu().conf
+                    error = 0
+                else:
+                    conf = 1
+                    error = 0
 
-            cv2.ellipse(frame, rb, (0, 255, 0), 2)
-            xo = int(roiBox[0] + roiBox[2] / 2)
-            yo = int(roiBox[1] + roiBox[3] / 2)
-            error = (roiBox[3])
+                cv2.rectangle(frame, (x_t_l, y_t_l), (x_b_r, y_b_r), (0, 255, 0), 2)
+
+            xo = int(selection_box[0] + selection_box[2] / 2)
+            yo = int(selection_box[1] + selection_box[3] / 2)
 
             print("Predicting trajectory...")
-            if yo < error or bgs.sum() < 50:
+            if bgs.sum() < 25 or conf < 0.05 or yo < error:
                 predicted, mu, statePost, errorCovPre = kf.predict(int(xo), int(yo))
                 mu, P = kf.kal(mu, P, B, u, z=None)
                 mm = False
@@ -145,6 +179,7 @@ def track_trajectory(input_file):
             if len(listCenterX) > 2:
                 res += [(mu, P)]
                 cv2.circle(frame, (predicted[0], predicted[1]), 10, (255, 0, 255), 3)
+
                 # Prediction #
                 mu2 = mu
                 P2 = P
@@ -178,7 +213,7 @@ def track_trajectory(input_file):
                     uncertainty = (xu[n] + yu[n]) / 2
                     cv2.circle(frame,
                                (int(xe[n]), int(ye[n])),
-                               5,
+                               int(uncertainty),
                                (255, 255, 0),
                                3)
 
@@ -219,14 +254,44 @@ def track_trajectory(input_file):
                 mu = np.array([0, 0, 0, 0])
                 P = np.diag([10, 10, 10, 10]) ** 2
 
-        cv2.imshow('Frame', frame)
+            if frame_num % FRAMES_INTERVAL == 0:
+                found = False
 
-    print('done')
+        if show:
+            cv2.imshow('Frame', frame)
+        frames.append(frame)
+
+    if write:
+        name = input_file[:-4] + "_tracked" + '.mp4'
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter(name, fourcc, 20.0, (frame_width, frame_height))
+        for frame in frames:
+            out.write(frame)
+    print("Done")
+    out.release()
     cap.release()
     cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    filename = f"data/01.mp4"
+    parser = argparse.ArgumentParser(description="Script for basketball shot trajectory prediction "
+                                                 "using YOLO and Kalman Filter")
+    parser.add_argument("--input", type=str, default="04.mp4",
+                        help="Name of the input video (must be in data folder)")
+    parser.add_argument("--model", type=str, default="medium",
+                        help="Type of model (nano, small, medium)")
+    parser.add_argument("--camshift", default=False, action="store_true",
+                        help="Whether to track the ball with camshift or YOLO")
+    parser.add_argument("--save-result", default=True, action="store_true",
+                        help="Save the resulting video with the predicted trajectory")
+    parser.add_argument("--show", default=False, action="store_true",
+                        help="Show procedure step-by-step")
+    args = parser.parse_args()
 
-    track_trajectory(filename)
+    filename = f"data/{args.input}"
+    model = args.model[0]
+    camshift_tracker = args.camshift
+    save_video = args.save_result
+    show = args.show
+
+    track_trajectory(filename, model, camshift_tracker, save_video, show)
